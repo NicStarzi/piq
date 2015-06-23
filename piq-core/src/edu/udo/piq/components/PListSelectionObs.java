59a3bde6b602b18fcc42ecceb4fca3d889530dca
Nic Starzi <NicStarzi@users.noreply.github.com>
2015-06-23 10:37:02 +0200

    Removed old PList and PTree
    These have been replaced by the new impementations.

--- piq-core/src/edu/udo/piq/components/PListSelectionObs.java
+++
@@ -1,9 +0,0 @@
-package edu.udo.piq.components;
-
-public interface PListSelectionObs {
-	
-	public void selectionAdded(PListSelection selection, Object element);
-	
-	public void selectionRemoved(PListSelection selection, Object element);
-	
-}
--- piq-core/src/edu/udo/piq/components/PTreeSelectionObs.java
+++
@@ -1,8 +0,0 @@
-package edu.udo.piq.components;
-
-public interface PTreeSelectionObs {
-	
-	public void selectionAdded(PTreeSelection selection, Object node);
-	
-	public void selectionRemoved(PTreeSelection selection, Object node);
-}

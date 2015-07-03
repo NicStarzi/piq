02fe923a436eaad4dc8378a710d890ae165b128e
Nic Starzi <NicStarzi@users.noreply.github.com>
2015-07-03 14:54:32 +0200

    Tutorial project started
    This project will contain example code that will be explained in detail
    as a tutorial that showcases all the features of piq.
    The tutorial does now include a PLayout that can contain a single
    component which moves in a circular motion based on time.

---
+++ piq-tutorial/src/edu/udo/piq/tutorial/MovingPLayout.java
@@ -0,0 +1,90 @@
+package edu.udo.piq.tutorial;
+
+import edu.udo.piq.PBounds;
+import edu.udo.piq.PComponent;
+import edu.udo.piq.PComponentObs;
+import edu.udo.piq.PLayoutObs;
+import edu.udo.piq.PReadOnlyLayout;
+import edu.udo.piq.PRoot;
+import edu.udo.piq.PSize;
+import edu.udo.piq.PTimer;
+import edu.udo.piq.PTimerCallback;
+import edu.udo.piq.tools.AbstractPLayout;
+import edu.udo.piq.tools.MutablePSize;
+
+public class MovingPLayout extends AbstractPLayout {
+	
+	protected final MutablePSize prefSize = new MutablePSize();
+	protected final PTimer timer = new PTimer(new PTimerCallback() {
+		public void action() {
+			time++;
+			fireInvalidateEvent();
+		}
+	});
+	private final PComponentObs ownerObs = new PComponentObs() {
+		public void rootChanged(PComponent component, PRoot currentRoot) {
+			if (currentRoot == null) {
+				timer.stop();
+			} else {
+				timer.start();
+			}
+		}
+	};
+	private PComponent content;
+	private int time;
+	private int radius = 100;
+	
+	public MovingPLayout(PComponent component) {
+		super(component);
+		addObs(new PLayoutObs() {
+			public void childAdded(PReadOnlyLayout layout, PComponent child, Object constraint) {
+				content = child;
+			}
+			public void childRemoved(PReadOnlyLayout layout, PComponent child, Object constraint) {
+				content = null;
+			}
+		});
+		component.addObs(ownerObs);
+		timer.setOwner(component);
+		timer.setRepeating(true);
+		timer.setDelay(1);
+	}
+	
+	public void dispose() {
+		getOwner().removeObs(ownerObs);
+		timer.stop();
+		timer.setOwner(null);
+	}
+	
+	protected boolean canAdd(PComponent component, Object constraint) {
+		return constraint == null;
+	}
+	
+	public void layOut() {
+		PBounds ob = getOwner().getBounds();
+		int centerX = ob.getCenterX();
+		int centerY = ob.getCenterY();
+		
+		int degree = time % (360 * timer.getDelay());
+		double rad = Math.toRadians(degree);
+		int circleX = centerX + (int) (radius * Math.cos(rad));
+		int circleY = centerY + (int) (radius * Math.sin(rad));
+		
+		PSize childSize = getPreferredSizeOf(content);
+		int childW = childSize.getWidth();
+		int childH = childSize.getHeight();
+		int childX = circleX - childW / 2;
+		int childY = circleY - childH / 2;
+		
+		setChildBounds(content, childX, childY, childW, childH);
+	}
+	
+	public PSize getPreferredSize() {
+		if (content == null) {
+			return PSize.NULL_SIZE;
+		} else {
+			return getPreferredSizeOf(content);
+		}
+	}
+	
+}
---
+++ piq-tutorial/src/edu/udo/piq/tutorial/TestMovingPLayout.java
@@ -0,0 +1,66 @@
+package edu.udo.piq.tutorial;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import edu.udo.piq.components.PButton;
+import edu.udo.piq.components.PButtonObs;
+import edu.udo.piq.components.PLabel;
+import edu.udo.piq.components.PPanel;
+import edu.udo.piq.swing.JCompPRoot;
+
+public class TestMovingPLayout {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestMovingPLayout window = new TestMovingPLayout();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private final JFrame frame;
+	private final JCompPRoot root;
+	
+	public TestMovingPLayout() {
+		frame = new JFrame();
+		frame.setSize(640, 480);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		root = new JCompPRoot();
+		frame.setContentPane(root.getPanel());
+		
+		Timer updateTimer = new Timer(10, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				root.update();
+			}
+		});
+		updateTimer.setCoalesce(true);
+		updateTimer.setRepeats(true);
+		updateTimer.start();
+		
+		PPanel bodyPnl = new PPanel();
+		bodyPnl.setLayout(new MovingPLayout(bodyPnl));
+		root.setBody(bodyPnl);
+		
+		PButton btn = new PButton();
+		btn.setContent(new PLabel("Click me!"));
+		btn.addObs(new PButtonObs() {
+			public void onClick(PButton button) {
+				System.out.println("You clicked it!");
+			}
+		});
+		bodyPnl.addChild(btn, null);
+	}
+	
+}

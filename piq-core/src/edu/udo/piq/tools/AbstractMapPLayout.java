d23c2f3f18214d5762ea0860b0439ab2367f9ccc
Nic Starzi <NicStarzi@users.noreply.github.com>
2015-12-11 14:06:50 +0100

    Fix for previous commit
    A previous commit did not include these abstract layouts.

---
+++ piq-core/src/edu/udo/piq/tools/AbstractMapPLayout.java
@@ -0,0 +1,42 @@
+package edu.udo.piq.tools;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.udo.piq.PComponent;
+import edu.udo.piq.PLayout;
+
+public abstract class AbstractMapPLayout extends AbstractPLayout implements PLayout {
+	
+	private final Map<PComponent, PCompInfo> compMap = new HashMap<>();
+	
+	protected AbstractMapPLayout(PComponent component) {
+		super(component);
+	}
+	
+	public Collection<PComponent> getChildren() {
+		return Collections.unmodifiableSet(compMap.keySet());
+	}
+	
+	protected PCompInfo getInfoFor(PComponent child) {
+		return compMap.get(child);
+	}
+	
+	protected Iterable<PCompInfo> getAllInfos() {
+		return compMap.values();
+	}
+	
+	protected void clearAllInfosInternal() {
+		compMap.clear();
+	}
+	
+	protected void addInfoInternal(PCompInfo info) {
+		compMap.put(info.comp, info);
+	}
+	
+	protected void removeInfoInternal(PCompInfo info) {
+		compMap.remove(info.comp);
+	}
+}
